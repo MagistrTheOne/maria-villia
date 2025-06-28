5bce26a543854f8ca0b07d21df04bea26e26e483
MagistrTheOne <maxonyushko71@gmail.com>
2025-06-28 21:29:56 +0300

    udpate

--- src/api/gallery-sections.js
+++ src/api/gallery-sections.js
@@ -1,9 +1,12 @@
 export async function fetchGallerySections() {
-  const res = await fetch("http://localhost:1337/api/gallery-sections?populate[Image][populate]=Image", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  const res = await fetch(
+    "https://balanced-dream-271ea111c9.strapiapp.com/api/gallery-sections?populate[Image][populate]=Image",
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch gallery sections");
