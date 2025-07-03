f230d73958ce9cba435b1d3ae2798558bbf9c865
MagistrTheOne <maxonyushko71@gmail.com>
2025-07-03 23:04:01 +0300

    босс я победил кажется

--- src/libs/fetchGallerySections.js
+++ src/libs/fetchGallerySections.js
@@ -11,17 +11,22 @@ export const fetchGallerySections = async () => {
     const items = entry.attributes?.imageupload || [];
 
     return {
-      title: items[0]?.Title || "Без названия",
-      images: items.map(item => ({
-        title: item.Title,
-        date: item.Data,
-        url:
-          item.Image?.formats?.medium?.url
-            ? BASE_URL + item.Image.formats.medium.url
-            : item.Image?.url
-            ? BASE_URL + item.Image.url
-            : "/",
-      })),
+      title: items[0]?.Title?.trim() || "Без названия",
+      images: items.map(item => {
+        const img = item?.Image;
+        const imgUrl =
+          img?.formats?.medium?.url
+            ? `${BASE_URL}${img.formats.medium.url}`
+            : img?.url
+            ? `${BASE_URL}${img.url}`
+            : null;
+
+        return {
+          title: item?.Title?.trim() || "Нет названия",
+          date: item?.Data || "Без даты",
+          url: imgUrl || "/fallback.jpg", // сюда можешь засунуть заглушку
+        };
+      }),
     };
   });
 };
